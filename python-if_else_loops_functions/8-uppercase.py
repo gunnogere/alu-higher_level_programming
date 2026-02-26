27663beb87ca8cf3193f8a9cc03297fde9b58e89
gunnogere@gmail.com <j.mulongo@alustudent.com>
2026-02-26 12:18:30 -0800

    Fix q8: You can only use no more than 2 print functions with string format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,5 @@
 def uppercase(str):
     """Print a string in uppercase followed by a new line"""
     for c in str:
-        if 'a' <= c <= 'z':
-            print("{}".format(chr(ord(c) - 32)), end='')
-        else:
-            print("{}".format(c), end='')
+        print("{}".format(chr(ord(c) - 32) if 'a' <= c <= 'z' else c), end='')
     print()
