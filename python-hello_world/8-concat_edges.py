d631042a4895c003011d7a9988171ee0e32e499a
gunnogere@gmail.com <j.mulongo@alustudent.com>
2026-02-26 11:46:53 -0800

    update slices for qtn. 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[52:76] + str[76:81] + str[:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[str.find('object-oriented'):str.find('programming')+11] + str[str.find('with'):str.find('with')+5] + str[str.find('Python'):str.find('Python')+6])
