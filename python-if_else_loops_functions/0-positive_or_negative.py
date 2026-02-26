eee0febabb711ed115f2b1808e788f60f2fba1a7
gunnogere@gmail.com <j.mulongo@alustudent.com>
2026-02-26 11:01:00 -0800

    Clean up Dir hireachy

---
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+
+if(number==0):
+	text="is zero"
+elif(number>0):
+	text="is positive"
+elif(number<0):
+	text="is negative"
+
+print(f"{number} {text}")
